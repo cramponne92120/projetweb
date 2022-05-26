262c920310c2499437149062c0f5d05c6382f35b
Thibault <cramponne@et.esiea.fr>
2022-05-26 15:10:21 +0200

    index new design

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Accueil</title>
+</head>
+<body>
+    <div class="menudiv">
+        <ul id="nav">
+        		    <div class="menu" id="logo"> <a href="index.php"> <p class="menup" style="color: white;"><img src="logo.jpeg" id="img"></p> </a> </div>
+        		    <div class="menu" id="connect"> <a href="connexion.php"> <p class="menup">Connexion</p> </a> </div>
+        	    	<div class="menu" id="signup"> <a href="creation"> <p class="menup">Créer un compte</p></div>
+            		<div class="menu" id="give"> <a href="https://www.paypal.com/paypalme/thibaultcramponne?country.x=FR&locale.x=fr_FR"> <p class="menup">Donation</p> </a> </div>
+        </ul>   
+    </div>
+    <div id="textblock">
+        <h2>Qui sommes nous ?</h2>
+        <p id="maintext">
+            Bienvenue sur le site de Haris et Thibault! Notre projet est de réaliser une "to do list" pour chaque utilisateur. Actuellement,
+            vous pouvez créer un compte et vous connecter pour tester notre application web. 
+            Une application mobile un peu plus poussée est en développement. Il s'agit d'une "to do list intelligente".
+            Néanmoins le projet mobile nécéssite encore un peu de temps. Si vous souhaitez accélérer le développement, vous pouvez vous rendre
+            sur l'onglet "Donation".  
+        </p> 
+    </div>
+</body>
+<footer>
+    <p id="footertext"> Site de Thibault&Haris </p>
+</footer>
+</html>
